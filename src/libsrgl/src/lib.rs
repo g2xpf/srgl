712fdfddf2bcde9fe4cf4f8dc62cff5be5c8d918
g2xpf <nameko.jack@gmail.com>
2020-04-05 05:31:09 +0900

    conditional compilation

--- src/libsrgl/src/lib.rs
+++ src/libsrgl/src/lib.rs
@@ -1,13 +1,25 @@
 #[cfg(feature = "default-backend")]
 pub struct World<G, R = libsrgl_reaction::Context, E = libsrgl_event::Context> {
-    global_resource: G,
-    reaction_context: R,
-    event_context: E,
+    pub global_resource: G,
+    pub reaction_context: R,
+    pub event_context: E,
 }
 
 #[cfg(not(feature = "default-backend"))]
 pub struct World<G, R, E> {
-    global_resource: G,
-    reaction_context: R,
-    event_context: E,
+    pub global_resource: G,
+    pub reaction_context: R,
+    pub event_context: E,
+}
+
+impl<G, R, E> World<G, R, E> {
+    pub fn new(global_resource: G, reaction_context: R, event_context: E) -> Self {
+        World {
+            global_resource,
+            reaction_context,
+            event_context,
+        }
+    }
+
+    pub fn run(&mut self) {}
 }
